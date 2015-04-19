7b84f86e7aee7e0239ce2df5fdb0f9654d799964
CapacitorSet <nobody@localhost>
2015-04-19 13:22:51 +0200

    Updated the authentication module

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 var express = require("express"),
 	app     = express(),
-	basicAuth = require('basic-auth-connect');
-
+	auth    = require("basic-auth"),
+	session = require("express-session");
+ 
 app.set('view engine', 'ejs'); 
-app.use(basicAuth(function(user, pass, callback) {
-	var result = (user === 'testUser' && pass === 'testPass');
-	callback(null /* error */, {user: user, pass: pass});
+app.use(function(req, res, next) {
+	var credentials = auth(req);
+	// If no credentials were provided, reject the request (this is needed for first-time hits).
+	if (!credentials) {
+		res.writeHead(401, {
+			'WWW-Authenticate': 'Basic realm="Node.js printing server"'
+		})
+	    res.end("Authentication failed! Please refresh the page and enter your data.");
+	    next("Authentication failed!");
+	}
+	// Else, continue processing the request, no matter what credentials were sent.
+	next();
+});
+
+app.use(session({
+	resave: false,
+	saveUninitialized: false,
+	secret: "Insert secret here"
 }));
 
 app.get("/", function(req, res) {
